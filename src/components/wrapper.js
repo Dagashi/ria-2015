a961837384ca4d5945d8c67822317bb370813498
David Strömbom <david.strombom@gmail.com>
2015-12-07 22:10:49 +0100

    Fixed wrapper so the sidebar and topbar is only rendered if the user is loggedin.

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -3,26 +3,50 @@ This is our top-level component. Sub-components matching specific routes will be
 contained in `this.props.children` and rendered out.
 */
 
-var React = require('react');
+var React = require('react'),
+	ReactRedux = require("react-redux"),
+	C = require("../constants");
 
 var Sidebar = require("./sidebar");
 var Topnav = require("./topnav");
 
 var Wrapper = React.createClass({
 	render: function() {
-		return (
-			<div className="main_container">
+		var content = "";
+		
+		//Only show Sidebar and topnav if loggedin
+		if(this.props.auth.currently === "LOGGED_IN") {
+			content = (
+				<div>
+					<Sidebar />
 
-				<Sidebar />
+					<Topnav />
 
-				<Topnav />
-
-				<div className="right_col" role="main">
+					<div className="right_col" role="main">
+						{this.props.children}
+					</div>
+				</div>
+			);
+		}
+		else {
+			content = (
+				<div role="main">
 					{this.props.children}
 				</div>
+			);
+		}
+		return (
+			<div className="main_container">
+				{content}
 			</div>
 		);
 	}
 });
 
-module.exports = Wrapper;
+// now we connect the component to the Redux store:
+var mapStateToProps = function(appstate){
+	// This component will have access to `appstate.auth` through `this.props.auth`
+	return {auth:appstate.auth};
+};
+
+module.exports = ReactRedux.connect(mapStateToProps)(Wrapper);
