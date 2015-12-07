77dcee197d013b9c25d492eee213f8220388f038
Jesper Håkansson <jesper@jesperh.se>
2015-12-07 15:39:00 +0100

    fix: Add auth actions to be exported and combine auth reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,13 +1,15 @@
-var Redux = require("redux"),
-	projectReducer = require("./projects");
-	taskReducer = require("./tasks");
+var Redux = require("redux");
+var projectReducer = require("./projects");
+var taskReducer = require("./tasks");
+var authReducer = require("./auth");
 
 /*
 In the future you might be adding more reducers here, but for now there is only 1 - the projects reducer.
 */
 var rootReducer = Redux.combineReducers({
 	projects: projectReducer, // this means projectReducer will operate on appState.projects
-	tasks: taskReducer // this means taskReducer will operate on appState.tasks
+	tasks: taskReducer, // this means taskReducer will operate on appState.tasks
+  auth: authReducer
 });
 
 module.exports = rootReducer;
